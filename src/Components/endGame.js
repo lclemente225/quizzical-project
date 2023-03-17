90eb382b96abe3fdebda9e4e64a2f4e0dac07951
Lawrence-Clemente <lawrence.clemente.cohort225@gmail.com>
2023-03-17 00:44:52 -0700

    update

--- src/Components/endGame.js
+++ src/Components/endGame.js
@@ -5,8 +5,9 @@ import {Link} from "react-router-dom"
    
         return (
             <div id="endgame">
-                <h1 id="endgameover" className="endgame-text">Congratulations</h1>
-                <p id="score" className="endgame-text">Your score is :{props.score}/10</p>
+               {props.score > 8 ? <h1 id="endgameover" className="endgame-text">Congratulations!</h1> : 
+                 <h1 id="endgameover" className="endgame-text">Better Luck Next Time!</h1>}
+                <p id="score" className="endgame-text">Your score is : {props.score} out of 10</p>
                 <Link to="/">
                 <button onClick={props.resetQuiz} id="endgame-button" className="endgame-text">Start Quiz Again?</button>
                 </Link>
